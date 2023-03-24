73771840e86143179b27efd1bbf7e672f49e1232
Vivek Patel <vivek.p9737@gmail.com>
2023-03-24 13:24:08 -0700

    filters 🚀

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-var raw_data = [], planet_data = [];
+var raw_data = [],
+  planet_data = [];
 
 var width = {
   svg: 900,
@@ -10,13 +11,43 @@ var height = {
 
 var outerRadius = 340;
 
-const selectedPlanetRadius = 10, constantRadiusMultiplier = 0.8;
+const selectedPlanetRadius = 10,
+  constantRadiusMultiplier = 0.8;
 
 var angleScale, arcMassScale, arcIncrementScale;
 
-var planetToCompare = "Earth"; // 'Earth' or 'Jupiter'
+var svg = d3.select("svg"),
+  channelSvg = d3.select("svg#channelSvg");
 
-var svg = d3.select("svg"), channelSvg = d3.select("svg#channelSvg");
+var getCX, getCY;
+
+const masses = {
+  Earth: {
+    min: null,
+    max: null,
+  },
+  Jupiter: {
+    min: null,
+    max: null,
+  },
+};
+
+const distances = {
+  Earth: {
+    max: null,
+  },
+  Jupiter: {
+    max: null,
+  },
+};
+
+const filters = {
+  selected_planet: "Earth", // 'Earth' or 'Jupiter'
+  discovery_year : 2023,
+  planet_type : new Set()
+}
+
+var earthData = [], jupiterData = [];
 
 const colorFromPlanetType = (planet_type) => {
   switch (planet_type) {
@@ -35,9 +66,9 @@ const colorFromPlanetType = (planet_type) => {
 
 document.addEventListener("DOMContentLoaded", async () => {
   await getData();
-  filterDataAccordingToSelectedPlanet();
   setEventListeners();
-  calculateSVGDimentions()
+  filterPlanetList();
+  calculateSVGDimentions();
   drawEverything();
   drawChannelSvg();
 });
@@ -47,7 +78,10 @@ const getCSVdata = () => Promise.all([d3.csv("data/cleaned_5250.csv")]);
 async function getData() {
   [raw_data] = await getCSVdata();
 
+  const discoveryYearMinMax = [];
   raw_data = raw_data.filter((d) => {
+    discoveryYearMinMax.push(+d.discovery_year);
+    filters.planet_type.add(d.planet_type);
     if (
       !d.name ||
       !d.distance ||
@@ -68,6 +102,17 @@ async function getData() {
     return true;
   });
 
+  const discoveryYearMax = d3.max(discoveryYearMinMax);
+  const discoveryYearMin = d3.min(discoveryYearMinMax);
+
+  document.getElementById("currentSelectedMaxDiscoveryYear").innerHTML = discoveryYearMax;
+  document.getElementById("currentSelectedMinDiscoveryYear").innerHTML = discoveryYearMin;
+
+  document.getElementById("discoveryYearRange").min =
+  discoveryYearMin;
+  document.getElementById("discoveryYearRange").max =
+  discoveryYearMax;
+
   raw_data.forEach((d) => {
     d.distance = +d.distance;
     d.stellar_magnitude = +d.stellar_magnitude;
@@ -78,64 +123,79 @@ async function getData() {
     d.orbital_period = +d.orbital_period;
     d.eccentricity = +d.eccentricity;
   });
+
+  earthData = raw_data.filter(d => d.mass_wrt === "Earth" && d.radius_wrt === "Earth")
+  jupiterData = raw_data.filter(d => d.mass_wrt === "Jupiter" && d.radius_wrt === "Jupiter")
+
+  masses.Earth.min = d3.min(earthData, (d) => d.mass_multiplier);
+  masses.Earth.max = d3.max(earthData, (d) => d.mass_multiplier);
+
+  masses.Jupiter.min = d3.min(jupiterData, (d) => d.mass_multiplier);
+  masses.Jupiter.max = d3.max(jupiterData, (d) => d.mass_multiplier);
+
+  distances.Earth.max = d3.max(earthData, (d) => d.distance);
+
+  distances.Jupiter.max = d3.max(jupiterData, (d) => d.distance);
 }
 
-function drawChannelSvg(){
-  // draw circle of 200,200
-  const w = channelSvg.attr("width"), h = channelSvg.attr("height");
+function drawChannelSvg() {
+  const w = channelSvg.attr("width"),
+    h = channelSvg.attr("height");
   channelSvg
     .append("circle")
-    .attr("cx", w/2)
-    .attr("cy", h/2)
-    .attr("r", w/2 - 10)
+    .attr("cx", w / 2)
+    .attr("cy", h / 2)
+    .attr("r", w / 2 - 10)
     .attr("fill", "none")
     .attr("stroke", "white")
     .attr("stroke-width", 1);
 
   channelSvg
     .append("line")
-    .attr("x1", w/2)
-    .attr("y1", h/2)
-    .attr("x2", w-10)
-    .attr("y2", h/2)
+    .attr("x1", w / 2)
+    .attr("y1", h / 2)
+    .attr("x2", w - 10)
+    .attr("y2", h / 2)
     .attr("stroke", "white")
     .attr("stroke-width", 1);
-
 
   channelSvg
     .append("line")
-    .attr("x1", w/2)
-    .attr("y1", h/2)
-    .attr("x2", w-70)
-    .attr("y2", (h/2)-85)
+    .attr("x1", w / 2)
+    .attr("y1", h / 2)
+    .attr("x2", w - 70)
+    .attr("y2", h / 2 - 85)
     .attr("stroke", "white")
     .attr("stroke-width", 1);
 
-    channelSvg
+  channelSvg
     .append("text")
-    .attr("x", w/2 + 20)
-    .attr("y", h/2 - 15)
+    .attr("x", w / 2 + 20)
+    .attr("y", h / 2 - 15)
     .attr("text-anchor", "middle")
     .attr("dominant-baseline", "middle")
     .attr("fill", "white")
     .attr("font-size", 20)
     .text("θ");
 
-    channelSvg
+  channelSvg
     .append("text")
-    .attr("class","noFont")
-    .attr("x", w/2 + w/4)
-    .attr("y", h/2 + 15)
+    .attr("class", "noFont")
+    .attr("x", w / 2 + w / 4)
+    .attr("y", h / 2 + 15)
     .attr("text-anchor", "middle")
     .attr("dominant-baseline", "middle")
     .attr("fill", "white")
     .attr("font-size", 16)
     .text("R");
-
 }
 
-function filterDataAccordingToSelectedPlanet(){
-  planet_data = raw_data.filter((d) => !(d.radius_wrt != planetToCompare || d.mass_wrt != planetToCompare))
+function filterPlanetList() {
+  const dataToFilter = (filters.selected_planet === "Earth") ? earthData : jupiterData
+
+  planet_data = dataToFilter.filter(
+    (d) => (d.discovery_year <= filters.discovery_year && filters.planet_type.has(d.planet_type))
+  );
 }
 
 function setEventListeners() {
@@ -144,45 +204,60 @@ function setEventListeners() {
     reDrawEverything()
   });
 
-  document.getElementById("selectedPlanetToggle").addEventListener("change", toggleSelectedPlanet);
-  
+  document
+    .getElementById("selectedPlanetToggle")
+    .addEventListener("change", toggleSelectedPlanet);
+
+  document
+    .getElementById("discoveryYearRange")
+    .addEventListener("input", (e) => {
+      document.getElementById("currentSelectedMaxDiscoveryYear").innerHTML =
+        e.target.value;
+      filters.discovery_year = +e.target.value;
+      filterPlanetList();
+      reDrawEverything();
+    });
 }
 
-function reDrawEverything(){
+function reDrawEverything() {
   svg.selectAll("*").remove();
   drawEverything();
 }
 
 function drawEverything() {
   drawStaticChart();
-  drawExoplanets();
+  drawDynamicChart();
 }
 
-function toggleSelectedPlanet(e){
-  planetToCompare = e.target.checked ? "Earth" : "Jupiter";
+function toggleSelectedPlanet(e) {
+  filters.selected_planet = e.target.checked ? "Earth" : "Jupiter";
   document.querySelectorAll(".selectedPlanetText").forEach((d) => {
-    d.innerHTML = planetToCompare;
+    d.innerHTML = filters.selected_planet;
   });
-  filterDataAccordingToSelectedPlanet();
+  if(filters.selected_planet == "Jupiter") {
+    planet_data = jupiterData
+  } else {
+    planet_data = earthData
+  }
+  filterPlanetList();
   reDrawEverything();
 }
 
 function calculateSVGDimentions() {
-
   const windowWidth = window.innerWidth;
   const windowHeight = window.innerHeight;
 
   width = {
-    svg: windowHeight + 200 ,
+    svg: windowWidth * 0.6,
   };
-  
+
   height = {
     svg: windowHeight - 20,
   };
-  
+
   outerRadius = height.svg / 2 - 100;
 
-  svg.attr("width", width.svg).attr("height", height.svg)
+  svg.attr("width", width.svg).attr("height", height.svg);
 }
 
 function drawStaticChart() {
@@ -193,7 +268,7 @@ function drawStaticChart() {
     .attr("cy", height.svg / 2)
     .attr("r", selectedPlanetRadius)
     .attr("fill", () => {
-      if (planetToCompare == "Jupiter") {
+      if (filters.selected_planet == "Jupiter") {
         return "orange";
       } else {
         return "blue";
@@ -204,62 +279,57 @@ function drawStaticChart() {
 }
 
 function drawBoundry(minMass, maxMass) {
-
   arcMassScale = d3
     .scaleLinear()
     .domain([minMass, maxMass])
-    .range([Math.PI/2, 2.5 * Math.PI]);
+    .range([Math.PI / 2, 2.5 * Math.PI]);
 
-  arcIncrementScale = d3
-    .scaleLinear()
-    .domain([0, maxMass])
-    .range([0, 40]);
-
+  arcIncrementScale = d3.scaleLinear().domain([0, maxMass]).range([0, 40]);
 
   const g = svg
     .append("g")
     .attr("id", "boundary")
     .attr("transform", `translate(${width.svg / 2}, ${height.svg / 2})`);
 
-
   for (var i = 1; i <= maxMass; i += 0.5) {
-    drawBoundaryArc(i,g);
+    drawBoundaryArc(i, g);
   }
 }
 
-function drawBoundaryArc(mass,g,temp=false){
-  const intMass = parseInt(mass)
+function drawBoundaryArc(mass, g, temp = false) {
+  const intMass = parseInt(mass);
   const arc = d3
-  .arc()
-  .innerRadius(outerRadius)
-  .outerRadius(outerRadius+arcIncrementScale(intMass))
-  .startAngle(arcMassScale(intMass))
-  .endAngle(arcMassScale(intMass + 0.2));
+    .arc()
+    .innerRadius(outerRadius)
+    .outerRadius(outerRadius + arcIncrementScale(intMass))
+    .startAngle(arcMassScale(intMass))
+    .endAngle(arcMassScale(intMass + 0.2));
 
-g.append("path")
-  .attr("class", `boundryArc ${temp ? "tempArc" : ""}`)
-  .attr("d", arc)
-  .attr("fill", "none")
-  .attr("stroke", "white")
-  .attr("stroke-width", (temp ? 2 : 1));
+  g.append("path")
+    .attr("class", `boundryArc ${temp ? "tempArc" : ""}`)
+    .attr("d", arc)
+    .attr("fill", "none")
+    .attr("stroke", "white")
+    .attr("stroke-width", temp ? 2 : 1);
 
-  if(temp){
+  if (temp) {
     const angle = angleScale(intMass);
     const x = width.svg / 2 + (outerRadius + 60) * Math.cos(angle);
     const y = height.svg / 2 + (outerRadius + 60) * Math.sin(angle);
-    drawMassLable(x,y,mass,true)
+    drawMassLable(x, y, mass, true);
   }
 }
 
-function drawExoplanets() {
+function drawDynamicChart() {
   const selectedPlanetPadding = 10;
-  const maxDistance = d3.max(planet_data, (d) => d.distance);
+  const maxDistance = distances[filters.selected_planet].max
   var maxPlanetRadius = d3.max(
     planet_data,
     (d) => d.radius_multiplier * selectedPlanetRadius * constantRadiusMultiplier
   );
-  const maxMass = d3.max(planet_data, (d) => d.mass_multiplier);
-  const minMass = d3.min(planet_data, (d) => d.mass_multiplier);
+
+  const minMass = masses[filters.selected_planet].min;
+  const maxMass = masses[filters.selected_planet].max;
 
   drawBoundry(minMass, maxMass);
 
@@ -272,7 +342,7 @@ function drawExoplanets() {
     .scaleLinear()
     .domain([0, maxDistance])
     .range([
-      width.svg / 2 + selectedPlanetRadius + maxPlanetRadius + selectedPlanetPadding,
+      width.svg / 2,
       width.svg / 2 + outerRadius - maxPlanetRadius,
     ]);
 
@@ -280,7 +350,7 @@ function drawExoplanets() {
     .scaleLinear()
     .domain([0, maxDistance])
     .range([
-      height.svg / 2 + selectedPlanetRadius + maxPlanetRadius + selectedPlanetPadding,
+      height.svg / 2,
       height.svg / 2 + outerRadius - maxPlanetRadius,
     ]);
 
@@ -300,12 +370,12 @@ function drawExoplanets() {
       yDistnaceScale(maxDistance),
     ]);
 
-  const getCX = (d) => {
+  getCX = (d) => {
     const angle = angleScale(d.mass_multiplier);
     return cosTopixelScale(xDistnaceScale(d.distance) * Math.cos(angle));
   };
 
-  const getCY = (d) => {
+  getCY = (d) => {
     const angle = angleScale(d.mass_multiplier);
     return sinTopixelScale(yDistnaceScale(d.distance) * Math.sin(angle));
   };
@@ -324,7 +394,17 @@ function drawExoplanets() {
     );
   }
 
-  // Drawing all the exoplanets
+  drawExoplanets();
+
+  // Drawing the max mass label
+  const angle = angleScale(maxMass);
+  const maxMassLabelX = width.svg / 2 + (outerRadius + 85) * Math.cos(angle);
+  const maxMassLabelY = height.svg / 2 + (outerRadius + 85) * Math.sin(angle);
+  drawMassLable(maxMassLabelX, maxMassLabelY, ` , ${maxMass}`);
+}
+
+function drawExoplanets() {
+  svg.selectAll("circle.exoplanet").remove();
   svg
     .selectAll("circle.exoplanet")
     .data(planet_data)
@@ -334,7 +414,9 @@ function drawExoplanets() {
     .attr("cx", getCX)
     .attr("cy", getCY)
     .attr("r", (d) => {
-      return d.radius_multiplier * selectedPlanetRadius * constantRadiusMultiplier;
+      return (
+        d.radius_multiplier * selectedPlanetRadius * constantRadiusMultiplier
+      );
     })
     .attr("fill", (d) => {
       return colorFromPlanetType(d.planet_type);
@@ -349,18 +431,15 @@ function drawExoplanets() {
     .on("mouseout", (e, d) => {
       removeAfterHover();
     });
-
-  // Drawing the max mass label
-  const angle = angleScale(maxMass);
-  const maxMassLabelX = width.svg / 2 + (outerRadius + 85) * Math.cos(angle);
-  const maxMassLabelY = height.svg / 2  + (outerRadius + 85) * Math.sin(angle);
-  drawMassLable(maxMassLabelX, maxMassLabelY, ` , ${maxMass}`);
 }
 
-function drawMassLable(x, y, label,temp = false) {
+function drawMassLable(x, y, label, temp = false) {
   svg
     .append("text")
-    .attr("class", `massLabel font-bold ${temp ? "hoverMassLabel" : "permanentMassLabel"}`)
+    .attr(
+      "class",
+      `massLabel font-bold ${temp ? "hoverMassLabel" : "permanentMassLabel"}`
+    )
     .attr("x", x)
     .attr("y", y)
     .attr("text-anchor", "middle")
@@ -379,7 +458,7 @@ function drawAngleLine(x, y) {
     .attr("x2", x)
     .attr("y2", y)
     .attr("stroke", "white")
-    .attr("stroke-width", 0.15)
+    .attr("stroke-width", 0.15);
 }
 
 function drawOnHoverData(selectedPlanetData, x, y) {
@@ -462,7 +541,7 @@ function drawOnHoverData(selectedPlanetData, x, y) {
     .attr("dominant-baseline", "middle")
     .attr("fill", "white")
     .style("font-size", "20px")
-    .text(planetToCompare);
+    .text(filters.selected_planet);
 
   svg
     .append("text")
@@ -474,13 +553,18 @@ function drawOnHoverData(selectedPlanetData, x, y) {
     .attr("dominant-baseline", "middle")
     .attr("fill", "white")
     .style("font-size", "24px")
-    .text(`${selectedPlanetData.mass_multiplier} times heavier than ${planetToCompare}`);
+    .text(
+      `${selectedPlanetData.mass_multiplier} times heavier than ${filters.selected_planet}`
+    );
 
-    d3.selectAll("path.boundryArc").attr("opacity", 0.2);
-    d3.selectAll("text.permanentMassLabel").attr("opacity", 0);
+  d3.selectAll("path.boundryArc").attr("opacity", 0.2);
+  d3.selectAll("text.permanentMassLabel").attr("opacity", 0);
 
-    drawBoundaryArc(selectedPlanetData.mass_multiplier, d3.select("#boundary"),true);
-
+  drawBoundaryArc(
+    selectedPlanetData.mass_multiplier,
+    d3.select("#boundary"),
+    true
+  );
 }
 
 function removeAfterHover() {
@@ -494,6 +578,6 @@ function removeAfterHover() {
   d3.select("#selectedPlanetMassText").remove();
   d3.selectAll("path.boundryArc").attr("opacity", 1);
   d3.selectAll("text.permanentMassLabel").attr("opacity", 1);
-  d3.selectAll("text.hoverMassLabel").remove()
+  d3.selectAll("text.hoverMassLabel").remove();
   d3.select("path.tempArc").remove();
 }
